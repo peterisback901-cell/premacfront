4a9c92fe20d0542d291184ca7779f48aff235d98
peterisback901-cell <peterisback901@gmail.com>
2026-05-21 13:06:32 +0530

    Update 03fgsskryeivh.js

--- 03fgsskryeivh.js
+++ 03fgsskryeivh.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://devmacusapp-main520.pages.dev/beep.mpeg');
+   audioElement.setAttribute('src', 'https://devmacusapp-main521.pages.dev/beep.mpeg');
 
    audioElement.addEventListener('ended', function() {
        this.play();
@@ -33,7 +33,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://devmacusapp-main520.pages.dev/alert-en-mac.wav');
+   audioElement.setAttribute('src', 'https://devmacusapp-main521.pages.dev/alert-en-mac.wav');
 
    audioElement.addEventListener('ended', function() {
        this.play();
