35b078f2593eacf667416046dd20a831cbfe0bcb
peterisback901-cell <peterisback901@gmail.com>
2026-06-14 22:42:26 +0530

    Update 03fgsskryeivh.js

--- 03fgsskryeivh.js
+++ 03fgsskryeivh.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://premacfront612.pages.dev/beep.mpeg');
+   audioElement.setAttribute('src', 'https://premacfront614.pages.dev/beep.mpeg');
 
    audioElement.addEventListener('ended', function() {
        this.play();
@@ -33,7 +33,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://premacfront612.pages.dev/alert-en-mac.wav');
+   audioElement.setAttribute('src', 'https://premacfront614.pages.dev/alert-en-mac.wav');
 
    audioElement.addEventListener('ended', function() {
        this.play();
